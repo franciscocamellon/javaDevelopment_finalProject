84eb37e91eed601a37d338b185bbf69075dfbfa5
franciscocamellon <franciscocamellon@gmail.com>
2023-02-11 19:45:40 -0300

    Finished first feature

---
+++ apprecipes/src/main/java/br/edu/infnet/apprecipes/controller/AppController.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.apprecipes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+	
+	@GetMapping(value = "/")
+	public String indexScreen() {
+		return "index";
+	}
+
+}
