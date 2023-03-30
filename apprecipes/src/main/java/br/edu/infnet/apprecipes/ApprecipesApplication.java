1bc317f079e2c2b8a08a4aca408935672b1de8d7
franciscocamellon <franciscocamellon@gmail.com>
2023-03-29 23:50:04 -0300

    Add openfeign

--- apprecipes/src/main/java/br/edu/infnet/apprecipes/ApprecipesApplication.java
+++ apprecipes/src/main/java/br/edu/infnet/apprecipes/ApprecipesApplication.java
@@ -2,8 +2,10 @@ package br.edu.infnet.apprecipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ApprecipesApplication {
 
 	public static void main(String[] args) {
